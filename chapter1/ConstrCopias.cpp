fffc203f8ccae83b0ccaf1a34364d5d0db271938
rebecaaras <rebecaaraujo2013@gmail.com>
2024-11-27 10:06:23 -0300

    Introduz método para sobrescrever operacao de atribuição

--- chapter1/ConstrCopias.cpp
+++ chapter1/ConstrCopias.cpp
@@ -15,14 +15,27 @@ struct Node {
         strcpy(nome, n.nome);
         idade = n.idade;
     }
+    
+	//Sobrescreve operação de atribuição
+    Node& operator=(const Node& n){
+        if (this != &n){//sem atribuição para si próprio;
+            if (nome != 0)
+            delete [] nome;
+            nome = new char[strlen(n.nome) + 1];
+            strcpy(nome, n.nome);
+            idade = n.idade;        
+        }
+        return *this;
+    };
 };
 
 int main(){
 	Node node1("Roger", 20);
 	Node node2(node1);
-	
 	strcpy(node2.nome, "Wendy");
 	node2.idade = 30;
+	
+	//node2 = node1; // a operação de atribuição precisa ser sobrescrita pelo usuário senão gera um resultado inesperado
 	
 	std::cout<< "Node 1: "<< node1.nome << ", "<<node1.idade << std::endl;
 	std::cout<< "Node 2: "<< node2.nome << ", "<<node2.idade << std::endl;
